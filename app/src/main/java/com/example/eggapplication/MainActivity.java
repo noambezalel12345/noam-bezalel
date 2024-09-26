a7f9829427d382c1d2419ce4081376654c5a12c5
noambezalel12345 <noambezalel2008@gmail.com>
2024-09-26 09:17:19 +0300

    bug in LinearLayoutCompat

--- app/src/main/java/com/example/eggapplication/MainActivity.java
+++ app/src/main/java/com/example/eggapplication/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         if(counter>0){
             counter--;
             TextView counterTextView = findViewById(R.id.textview_counter);
-            counterTextView.setText(counter);
+            counterTextView.setText(""+counter);
             if(counter==0){
                 ImageView eggImageView = findViewById(R.id.imageview_egg);
                 eggImageView.setImageResource(R.drawable.dog_1);
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     public void reset(View view) {
         counter = 10;
         TextView counterTextView = findViewById(R.id.textview_counter);
-        counterTextView.setText(counter);
+        counterTextView.setText(""+counter);
         ImageView eggImageView = findViewById(R.id.imageview_egg);
         eggImageView.setImageResource(R.drawable.egg_2);
     }
