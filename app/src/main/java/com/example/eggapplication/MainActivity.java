ef14f23bcc2ccacc046d8a1e482fbb4247b74c7e
noambezalel12345 <noambezalel2008@gmail.com>
2024-10-10 10:59:06 +0300

    bug in LinearLayoutCompat

--- app/src/main/java/com/example/eggapplication/MainActivity.java
+++ app/src/main/java/com/example/eggapplication/MainActivity.java
@@ -6,32 +6,48 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
+
 
     private int counter;
     private Button btnGetOut;
-    //private void EvantHandler(){
-        //btnGetOut.setOnClickListener(new View.onClickListener()){
-        //public void onClick(View v){
-           // }
-        // }
+    private String Tag;
 
+    private void EvantHandler(){
+
+        btnGetOut.setOnClickListener(new View.onClickListener(){
+            public void onClick(View v){
+                AlertDialog.Builder Builder=new AlertDialog.Builder(MainActivity.this);
+                Builder.setMessage("Are you sure you wanna leave?")
+                        //////////////////
+            finish();
+            }
+        });
+    }
 
     @Override
     public <T extends View> T findViewById(int id) {
         return super.findViewById(id);
     }
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Log.e(Tag, "MainActivity::OnCreate!!!!");
+
+        EdgeToEdge.enable(this);
         counter = 10;
         TextView counterTextView = findViewById(R.id.textview_counter);
         counterTextView.setText(""+counter);
@@ -63,5 +79,6 @@ public class MainActivity extends AppCompatActivity {
         counterTextView.setText(""+counter);
         ImageView eggImageView = findViewById(R.id.imageview_egg);
         eggImageView.setImageResource(R.drawable.egg_2);
+
     }
 }
